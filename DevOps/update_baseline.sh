a762e473d219bf9f03c02d82b4f7c76a0e22d94b
Chris Quevedo <chris.quevedo@yahoo.com>
2022-09-16 20:25:51 -0700

    Update baseline

--- DevOps/update_baseline.sh
+++ DevOps/update_baseline.sh
@@ -2,8 +2,8 @@
 
 echo "Updating Baseline"
 cd $(git rev-parse --show-toplevel)
-DP_errors=(find src/DP -type f)
-IP_errors=(find src/IP -type f)
+DP_errors=$(find src/DP -type f)
+IP_errors=$(find src/IP -type f)
 
 echo ${DP_errors} > Baseline/DP/index.txt
 echo ${IP_errors} > Baseline/IP/index.txt
